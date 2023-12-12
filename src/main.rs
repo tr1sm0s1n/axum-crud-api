081e9d789c96aa49b0d560e5c95cce4644498fff
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-12-12 21:52:38 +0530

    ✨ | added api

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!(">> Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello, World!"
+}
