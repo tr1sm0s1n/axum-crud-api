7d0bd25e660e2f7a637083588b7089159aa32a90
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-12-22 15:19:13 +0530

    🧪 | added test for 'create_one'

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,15 +1,16 @@
 use axum::{
     body::Body,
-    http::{Request, StatusCode},
+    http::{self, Request, StatusCode},
 };
 
 use http_body_util::BodyExt; // for `collect`
+use serde_json::{json, Value};
 use tower::ServiceExt; // for `call`, `oneshot`, and `ready`
 
 use crate::app;
 
 #[tokio::test]
-async fn home() {
+async fn home_router() {
     let app = app();
 
     let response = app
@@ -21,4 +22,30 @@ async fn home() {
 
     let body = response.into_body().collect().await.unwrap().to_bytes();
     assert_eq!(&body[..], b"Hello, World!");
+}
+
+#[tokio::test]
+async fn create_router() {
+    let app = app();
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/create")
+                .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                .body(Body::from(serde_json::to_vec(&json!({ "id": 56, "name": "Eirene", "course": "MBCC", "status": true, "date": "21-12-2022" })).unwrap()))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    assert_eq!(
+        body,
+        json!({ "id": 56, "name": "Eirene", "course": "MBCC", "status": true, "date": "21-12-2022" })
+    );
 }
