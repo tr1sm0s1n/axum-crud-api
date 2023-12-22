d53784d6a0203fc6c7a8ad10028052fdf1c86c10
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-12-22 11:34:24 +0530

    ♻️ | refactored logger

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,11 @@ mod handlers;
 mod models;
 mod types;
 
-use std::time::Duration;
-
 use axum::{
-    error_handling::HandleErrorLayer,
-    http::StatusCode,
     routing::{delete, get, post, put},
     Router,
 };
 use handlers::routes;
-use tower::{BoxError, ServiceBuilder};
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -37,29 +32,15 @@ async fn main() {
         .route("/read/:id", get(routes::read_one))
         .route("/update/:id", put(routes::update_one))
         .route("/delete/:id", delete(routes::delete_one))
-        .layer(
-            ServiceBuilder::new()
-                .layer(HandleErrorLayer::new(|error: BoxError| async move {
-                    if error.is::<tower::timeout::error::Elapsed>() {
-                        Ok(StatusCode::REQUEST_TIMEOUT)
-                    } else {
-                        Err((
-                            StatusCode::INTERNAL_SERVER_ERROR,
-                            format!("Unhandled internal error: {error}"),
-                        ))
-                    }
-                }))
-                .timeout(Duration::from_secs(10))
-                .layer(TraceLayer::new_for_http())
-                .into_inner(),
-        )
+        .layer(TraceLayer::new_for_http())
         .with_state(db);
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
-    println!(">> Listening on {}", listener.local_addr().unwrap());
+    // println!(">> Listening on {}", listener.local_addr().unwrap());
+    tracing::debug!("Listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
