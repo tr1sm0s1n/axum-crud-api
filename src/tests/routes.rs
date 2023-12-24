5a347e050f5fa5e9d0a7013c53344ec5e602548f
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-12-24 10:48:04 +0000

    🧪 | added test for 'read_one'

--- src/tests/routes.rs
+++ src/tests/routes.rs
@@ -5,7 +5,7 @@ use axum::{
 
 use http_body_util::BodyExt; // for `collect`
 use serde_json::{json, Value};
-use tower::ServiceExt; // for `call`, `oneshot`, and `ready`
+use tower::{Service, ServiceExt}; // for `call`, `oneshot`, and `ready`
 
 use crate::app;
 
@@ -17,7 +17,6 @@ async fn home() {
         .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
         .await
         .unwrap();
-
     assert_eq!(response.status(), StatusCode::OK);
 
     let body = response.into_body().collect().await.unwrap().to_bytes();
@@ -37,8 +36,8 @@ async fn not_found() {
         )
         .await
         .unwrap();
-
     assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
     let body = response.into_body().collect().await.unwrap().to_bytes();
     assert!(body.is_empty());
 }
@@ -53,12 +52,12 @@ async fn create_one() {
                 .method(http::Method::POST)
                 .uri("/create")
                 .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-                .body(Body::from(serde_json::to_vec(&json!({ "id": 56, "name": "Eirene", "course": "MBCC", "status": true, "date": "21-12-2022" })).unwrap()))
+                .body(Body::from(serde_json::to_vec(
+                    &json!({ "id": 56, "name": "Eirene", "course": "MBCC", "status": true, "date": "21-12-2022" })).unwrap()))
                 .unwrap(),
         )
         .await
         .unwrap();
-
     assert_eq!(response.status(), StatusCode::CREATED);
 
     let body = response.into_body().collect().await.unwrap().to_bytes();
@@ -77,6 +76,44 @@ async fn read_all() {
         .oneshot(Request::builder().uri("/read").body(Body::empty()).unwrap())
         .await
         .unwrap();
-
     assert_eq!(response.status(), StatusCode::OK);
 }
+
+#[tokio::test]
+async fn read_one() {
+    let mut app = app().into_service();
+
+    let request = Request::builder()
+    .method(http::Method::POST)
+    .uri("/create")
+    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+    .body(Body::from(serde_json::to_vec(
+        &json!({ "id": 36, "name": "Raven", "course": "MBCC", "status": true, "date": "24-12-2023" })).unwrap())).unwrap();
+    let response = ServiceExt::<Request<Body>>::ready(&mut app)
+        .await
+        .unwrap()
+        .call(request)
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::CREATED);
+
+    let request = Request::builder()
+        .uri("/read/36")
+        .body(Body::empty())
+        .unwrap();
+    let response = ServiceExt::<Request<Body>>::ready(&mut app)
+        .await
+        .unwrap()
+        .call(request)
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    assert_eq!(
+        body,
+        json!({ "id": 36, "name": "Raven", "course": "MBCC", "status": true, "date": "24-12-2023" })
+    );
+}
+
